dddf414436935fb76c699a84ff9f000718d87be6
Amrrish1209 <roshanbabubl@gmail.com>
2023-04-16 09:45:04 +0530

    Add:Remove avoidable word from the phrase “Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations”

--- MyHashTable.java
+++ MyHashTable.java
@@ -4,88 +4,70 @@ import java.util.LinkedList;
 
 public class MyHashTable {
 
-	private int capacity;
-	private LinkedList<MyMapNode>[] buckets;
+	private final int numBuckets;
+	private final LinkedList<MyMapNode>[] buckets;
 
 	public MyHashTable() {
-		this.capacity = 10;
-		this.buckets = new LinkedList[capacity];
-		for (int i = 0; i < capacity; i++) {
-			buckets[i] = new LinkedList<>();
-		}
+		this.numBuckets = 10;
+		this.buckets = new LinkedList[numBuckets];
 	}
 
 	private int getBucketIndex(String key) {
 		int hashCode = Math.abs(key.hashCode());
-		return hashCode % capacity;
+		return hashCode % numBuckets;
 	}
 
 	public int get(String key) {
 		int bucketIndex = getBucketIndex(key);
 		LinkedList<MyMapNode> bucket = buckets[bucketIndex];
-		for (MyMapNode node : bucket) {
-			if (node.key.equals(key)) {
+		if (bucket == null)
+			return 0;
+		MyMapNode node = bucket.peek();
+		while (node != null) {
+			if (node.key.equals(key))
 				return node.value;
-			}
+			node = node.next;
 		}
-		return -1;
+		return 0;
 	}
 
-	public void put(String key, int value) {
+	public void add(String key, int value) {
 		int bucketIndex = getBucketIndex(key);
 		LinkedList<MyMapNode> bucket = buckets[bucketIndex];
-		for (MyMapNode node : bucket) {
+		if (bucket == null) {
+			bucket = new LinkedList<MyMapNode>();
+			buckets[bucketIndex] = bucket;
+		}
+		MyMapNode node = bucket.peek();
+		while (node != null) {
 			if (node.key.equals(key)) {
 				node.value = value;
 				return;
 			}
+			node = node.next;
 		}
-		bucket.add(new MyMapNode(key, value));
+		MyMapNode newNode = new MyMapNode(key, value);
+		newNode.next = bucket.peek();
+		bucket.push(newNode);
 	}
 
 	public void remove(String key) {
 		int bucketIndex = getBucketIndex(key);
 		LinkedList<MyMapNode> bucket = buckets[bucketIndex];
-		for (MyMapNode node : bucket) {
+		if (bucket == null)
+			return;
+		MyMapNode prevNode = null;
+		MyMapNode node = bucket.peek();
+		while (node != null) {
 			if (node.key.equals(key)) {
-				bucket.remove(node);
+				if (prevNode == null)
+					bucket.pop();
+				else
+					prevNode.next = node.next;
 				return;
 			}
-		}
-	}
-
-	public void recalculateFrequencies(String phrase) {
-		// Remove the word "avoidable" from the hash table
-		int indexToRemove = Math.abs("avoidable".hashCode() % capacity);
-		MyMapNode previous = null;
-		MyMapNode current = buckets[indexToRemove].getFirst();
-		while (current != null) {
-			if (current.key.equals("avoidable")) {
-				if (previous == null) {
-					buckets[indexToRemove].removeFirst();
-				} else {
-					buckets[indexToRemove].remove(current);
-				}
-				break;
-			}
-			previous = current;
-			current = current.next;
-		}
-
-		// Recalculate the frequencies
-		String[] words = phrase.split(" ");
-		for (int i = 0; i < capacity; i++) {
-			LinkedList<MyMapNode> bucket = buckets[i];
-			for (int j = 0; j < bucket.size(); j++) {
-				MyMapNode node = bucket.get(j);
-				node.value = 0;
-				for (String word : words) {
-					if (node.key.equals(word)) {
-						node.value++;
-					}
-				}
-				System.out.println(node.key + ": " + node.value);
-			}
+			prevNode = node;
+			node = node.next;
 		}
 	}
 
@@ -93,16 +75,22 @@ public class MyHashTable {
 		String phrase = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
 		String[] words = phrase.split(" ");
 
-		MyHashTable map = new MyHashTable();
+		MyHashTable wordFrequency = new MyHashTable();
 		for (String word : words) {
-			int frequency = map.get(word);
-			if (frequency == -1) {
-				map.put(word, 1);
-			} else {
-				map.put(word, frequency + 1);
-			}
+			int frequency = wordFrequency.get(word);
+			wordFrequency.add(word, frequency + 1);
 		}
 
-		map.recalculateFrequencies(phrase);
+		System.out.println("Before removing 'avoidable':");
+		for (String word : words) {
+			System.out.println(word + ": " + wordFrequency.get(word));
+		}
+
+		wordFrequency.remove("avoidable");
+
+		System.out.println("\nAfter removing 'avoidable':");
+		for (String word : words) {
+			System.out.println(word + ": " + wordFrequency.get(word));
+		}
 	}
 }
