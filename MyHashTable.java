75310d49c49454b25cdd62fdf8c9d047f2ec8710
Amrrish1209 <roshanbabubl@gmail.com>
2023-04-16 09:42:55 +0530

    Add:Remove avoidable word from the
    phrase “Paranoids are not paranoid
    
    because they are paranoid but
    
    because they keep putting themselves
    
    deliberately into paranoid avoidable
    
    situations”

--- MyHashTable.java
+++ MyHashTable.java
@@ -4,68 +4,105 @@ import java.util.LinkedList;
 
 public class MyHashTable {
 
-	private LinkedList<MyMapNode>[] bucketArray;
-	private int numBuckets;
-	private int size;
+	private int capacity;
+	private LinkedList<MyMapNode>[] buckets;
 
 	public MyHashTable() {
-		numBuckets = 31; // can be any prime number
-		bucketArray = new LinkedList[numBuckets];
-		for (int i = 0; i < numBuckets; i++) {
-			bucketArray[i] = new LinkedList<MyMapNode>();
+		this.capacity = 10;
+		this.buckets = new LinkedList[capacity];
+		for (int i = 0; i < capacity; i++) {
+			buckets[i] = new LinkedList<>();
 		}
 	}
 
 	private int getBucketIndex(String key) {
-		int hashCode = key.hashCode();
-		int index = Math.abs(hashCode) % numBuckets;
-		return index;
-	}
-
-	private MyMapNode getNode(LinkedList<MyMapNode> list, String key) {
-		for (MyMapNode node : list) {
-			if (node.key.equals(key)) {
-				return node;
-			}
-		}
-		return null;
-	}
-
-	public void put(String key, int value) {
-		int bucketIndex = getBucketIndex(key);
-		LinkedList<MyMapNode> list = bucketArray[bucketIndex];
-		MyMapNode node = getNode(list, key);
-		if (node == null) {
-			node = new MyMapNode(key, value);
-			list.addLast(node);
-			size++;
-		} else {
-			node.value = value;
-		}
+		int hashCode = Math.abs(key.hashCode());
+		return hashCode % capacity;
 	}
 
 	public int get(String key) {
 		int bucketIndex = getBucketIndex(key);
-		LinkedList<MyMapNode> list = bucketArray[bucketIndex];
-		MyMapNode node = getNode(list, key);
-		return node == null ? 0 : node.value;
+		LinkedList<MyMapNode> bucket = buckets[bucketIndex];
+		for (MyMapNode node : bucket) {
+			if (node.key.equals(key)) {
+				return node.value;
+			}
+		}
+		return -1;
+	}
+
+	public void put(String key, int value) {
+		int bucketIndex = getBucketIndex(key);
+		LinkedList<MyMapNode> bucket = buckets[bucketIndex];
+		for (MyMapNode node : bucket) {
+			if (node.key.equals(key)) {
+				node.value = value;
+				return;
+			}
+		}
+		bucket.add(new MyMapNode(key, value));
+	}
+
+	public void remove(String key) {
+		int bucketIndex = getBucketIndex(key);
+		LinkedList<MyMapNode> bucket = buckets[bucketIndex];
+		for (MyMapNode node : bucket) {
+			if (node.key.equals(key)) {
+				bucket.remove(node);
+				return;
+			}
+		}
+	}
+
+	public void recalculateFrequencies(String phrase) {
+		// Remove the word "avoidable" from the hash table
+		int indexToRemove = Math.abs("avoidable".hashCode() % capacity);
+		MyMapNode previous = null;
+		MyMapNode current = buckets[indexToRemove].getFirst();
+		while (current != null) {
+			if (current.key.equals("avoidable")) {
+				if (previous == null) {
+					buckets[indexToRemove].removeFirst();
+				} else {
+					buckets[indexToRemove].remove(current);
+				}
+				break;
+			}
+			previous = current;
+			current = current.next;
+		}
+
+		// Recalculate the frequencies
+		String[] words = phrase.split(" ");
+		for (int i = 0; i < capacity; i++) {
+			LinkedList<MyMapNode> bucket = buckets[i];
+			for (int j = 0; j < bucket.size(); j++) {
+				MyMapNode node = bucket.get(j);
+				node.value = 0;
+				for (String word : words) {
+					if (node.key.equals(word)) {
+						node.value++;
+					}
+				}
+				System.out.println(node.key + ": " + node.value);
+			}
+		}
 	}
 
 	public static void main(String[] args) {
-		String sentence = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+		String phrase = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+		String[] words = phrase.split(" ");
+
 		MyHashTable map = new MyHashTable();
-
-		// split the sentence into words and add them to the hash table
-		String[] words = sentence.split(" ");
 		for (String word : words) {
 			int frequency = map.get(word);
-			map.put(word, frequency + 1);
+			if (frequency == -1) {
+				map.put(word, 1);
+			} else {
+				map.put(word, frequency + 1);
+			}
 		}
 
-		// print the frequency of each word in the sentence
-		for (String word : words) {
-			int frequency = map.get(word);
-			System.out.println(word + ": " + frequency);
-		}
+		map.recalculateFrequencies(phrase);
 	}
 }
